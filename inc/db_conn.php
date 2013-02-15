e87e02ad3795b1b8f13257a986b830656f237d8b
Jim Ivanoff <jim@polarbeardesign.net>
2013-02-15 13:36:40 -0600

    registration database setting update

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -4,4 +4,6 @@
 $db_name = "scipy";
 $connection = @mysql_connect("10.3.0.20","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
 
+$db = @mysql_select_db($db_name, $connection) or die("Couldn't select database.");
+
 ?>
