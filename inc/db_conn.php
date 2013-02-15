73ea3b9fdbd09a1dd09c4f752eb828949020e0d2
Jim Ivanoff <jim@polarbeardesign.net>
2013-02-15 11:53:08 -0600

    update db connection info

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -1,12 +1,12 @@
 <?php
 
 // LIVE SITE
-//$db_name = "scipy";
-//$connection = @mysql_connect("10.3.0.20","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
+$db_name = "scipy";
+$connection = @mysql_connect("10.3.0.20","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
 
 // LOCAL
-$db_name = "scipy";
-$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect to database.");
+//$db_name = "scipy";
+//$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect to database.");
 
 // PREVIEW SITE
 //$db_name = "polarbea_scipy";
