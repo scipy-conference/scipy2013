341a26f2a7d10734f1494a3fe2b647d199b675c2
Jim Ivanoff <jim@polarbeardesign.net>
2013-02-26 15:50:46 -0600

    updated call for tutorials

--- tutorial_overview.php
+++ tutorial_overview.php
@@ -102,8 +102,8 @@ limited. Tutorials will be 4 hours in duration and will be assigned to
 one of the three tracks.
 
 For examples of content and format, you can refer to past tutorials
-from past SciPy tutorial sessions ([SciPy2012][1], [SciPy2011][2],
-[SciPy2010][3]). We are looking for awesome techniques or packages,
+from past SciPy tutorial sessions ([SciPy2012]_, [SciPy2011]_,
+[SciPy2010]_). We are looking for awesome techniques or packages,
 helping new or advanced python programmers develop better or faster
 scientific applications.
 
@@ -113,9 +113,8 @@ from one of the 4 topics listed below:
 
 * Introduction Scientific Python Basics (Numpy and IPython)
 * Introduction to plotting with Matplotlib
-* Introduction to Scipy: optimization, linear algebra, statistics, and
-  more
-* Introduction the data analysis with Pandas
+* Overview of Scipy
+* Software Carpentry
 
 
 Stipend
@@ -129,28 +128,29 @@ stipend may increase to $1000 depending on availability of funds.
 Proposal Submission
 -------------------
 
-To submit your tutorial proposal, please send the following materials
-to tutorials@scipy.org:
+To submit your tutorial proposal, please go to:
+http://conference.scipy.org/scipy2013/tutorial_submission.php and fill
+the form, or in case you prefer so, send the materials to
+tutorials@scipy.org.  You will need the next:
 
 * A short bio of the presenter or team members, containing a
   description of past experiences as a trainer/teacher/speaker, and
   (ideally) links to videos of these experiences if available.
 * Which track the tutorial would fit best in Intermediate or Advanced
   if an open submisssion, or which of the 4 introductory topics
-  otherwise
-* A (~15 lines) description of the tutorial, suitable for posting on
-  the SciPy website for attendees to view. It should include the
-  target audience, the expected level of knowledge prior to the class,
-  and the goals of the class.
+  otherwise.
+* A description of the tutorial, suitable for posting on the SciPy
+  website for attendees to view. It should include the target
+  audience, the expected level of knowledge prior to the class, and
+  the goals of the class.
 * A more detailed outline of the tutorial content, including the
-  duration of each part, and exercise sessions for the review
-  process. Please include a description of how you plan to make the
-  tutorial hands-on.
+  duration of each part, and exercise sessions. Please include a
+  description of how you plan to make the tutorial hands-on.
 * A list of Python packages that attendees will need to have installed
   prior to the class to follow along. Please mention if any packages
   are not cross platform. Installation instructions or links to
   installation documentation should be provided for packages that are
-  not available through easy_install, pip, EPD, Anaconda CE etc., or
+  not available through easy_install, pip, EPD, Anaconda, etc., or
   that require third party or compiled libraries.
 * If available, the tutorial notes, slides, exercise files, ipython
   notebooks, that you already have, even if they are preliminary.
@@ -160,18 +160,21 @@ Selection
 
 Accepted tutorials will be announced on April 15th. Final tutorial
 materials and instructions for attendees will be due on May 24th. This
-will include final version numbers of required software to ensure that
-attendees have sufficient time to prepare before the conference.
+will include final version numbers of required software and a test 
+script that can be run by attendees to ensure that they have 
+sufficient time to prepare their laptops before the conference.
 
 Important dates:
 ----------------
 
-* Feb 18th:	Calls for tutorial submissions
-* Mar 18th:	Tutorial submissions due 
+* Feb 27th:	Calls for tutorial submissions
+* Apr  1st:	Tutorial submissions due 
 * Apr 15th:	Accepted tutorials announced
 * May  1st:	Speaker and Schedule announced
 * May  6th:	Early registration ends
-* May 24th: Final submission of tutorial materials and software version numbers
+* May 24th: Final submission of tutorial materials, software version 
+  numbers and test scripts.
+
 * Monday-Tuesday, June 24 - 25: SciPy 2013 Tutorials, Austin TX
 * Wednesday-Thursday, June 26 - 27: SciPy 2013 Conference, Austin TX
 * Friday-Saturday, June 27 - 28: SciPy 2013 Sprints, Austin TX & remote
@@ -184,9 +187,9 @@ The SciPy 2013 Tutorial Chairs
 * Francesc Alted, Software Architect at Continuum Analytics Inc.
 * Dharhas Pothina, Water Informatics Lead at the Texas Water Development Board
 
-[1]: http://conference.scipy.org/scipy2012/tutorials.php
-[2]: http://conference.scipy.org/scipy2011/tutorials.php
-[3]: http://conference.scipy.org/scipy2010/tutorials.html") ?>
+[SciPy2012]: http://conference.scipy.org/scipy2012/tutorials.php
+[SciPy2011]: http://conference.scipy.org/scipy2011/tutorials.php
+[SciPy2010]: http://conference.scipy.org/scipy2010/tutorials.html") ?>
 
 </section>
 
