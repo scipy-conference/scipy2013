0e528e3865a78f4e4d0e2fd93f4e00525906d880
Jim Ivanoff <jim@polarbeardesign.net>
2013-03-01 12:02:25 -0600

    repositions facebook like button js on fin aid page

--- reg_fin_aid.php
+++ reg_fin_aid.php
@@ -113,16 +113,6 @@ mail("$to", "$subject", "$message", "$headers");
 
 <body>
 
-<!-- for facebook like button -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 <div id="container">
 
 <?php include('inc/page_headers.php') ?>
@@ -236,6 +226,16 @@ $(document).ready(function()
 <link rel="shortcut icon" href="http://conference.scipy.org/scipy2013/favicon.ico" />
 
 </head>
+
+<!-- for facebook like button -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 <body>
 
