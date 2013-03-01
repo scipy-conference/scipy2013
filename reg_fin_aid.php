64481abf2ea0054f36c0df57e469958dc6ca00e4
Jim Ivanoff <jim@polarbeardesign.net>
2013-03-01 12:00:16 -0600

    tweaks to g-plus code for social

--- reg_fin_aid.php
+++ reg_fin_aid.php
@@ -113,6 +113,16 @@ mail("$to", "$subject", "$message", "$headers");
 
 <body>
 
+<!-- for facebook like button -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="container">
 
 <?php include('inc/page_headers.php') ?>
@@ -361,6 +371,15 @@ Word Count : <span id="display_count">0</span></p>
 <?php include('inc/page_footer.php') ?>
 </footer>
 </div>
+
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
 </body>
 
 </html>
--- sponsor_overview.php
+++ sponsor_overview.php
@@ -15,6 +15,16 @@
 
 <body>
 
+<!-- for facebook like button -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="container">
 
 <?php include('inc/page_headers.php') ?>
@@ -76,6 +86,14 @@
 <?php include('inc/page_footer.php') ?>
 </footer>
 </div>
+
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
 </body>
 
 </html>
