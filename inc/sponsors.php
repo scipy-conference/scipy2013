8430f510130ae8ff78942e5fbf07ca079a946c40
Jim Ivanoff <jim@polarbeardesign.net>
2013-04-09 10:22:16 -0500

    added height to sponsor images

--- inc/sponsors.php
+++ inc/sponsors.php
@@ -10,44 +10,44 @@
 
 <h3 class="sponsor_level">Institutional Sponsor</h3>
 <div class="sponsor_cell">
-  <a href="http://www.enthought.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/enthought.png" alt="Enthought, Inc." width="250"  class="sponsor_logo" /></a>
+  <a href="http://www.enthought.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/enthought.png" alt="Enthought, Inc." width="251" height="56" class="sponsor_logo" /></a>
 </div>
 
 <h3 class="sponsor_level">Platinum Sponsor</h3>
 <div class="sponsor_cell">
-  <a href="http://www.google.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/Google.png" alt="Google" width="184" class="sponsor_logo" /></a>
+  <a href="http://www.google.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/Google.png" alt="Google" width="180" height="64" class="sponsor_logo" /></a>
 </div>
 
 <h3 class="sponsor_level">Silver Sponsors</h3>
 <div class="sponsor_cell">
-  <a href="http://continuum.io/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/continuum_logo.jpg" alt="Continuum" width="178" class="sponsor_logo" /></a>
+  <a href="http://continuum.io/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/continuum_logo.jpg" alt="Continuum" width="178" height="86" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://github.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/github.gif" alt="github" width="180" class="sponsor_logo" /></a>
+  <a href="http://github.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/github.gif" alt="github" width="180" height="79" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://www.kitware.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/kitware.png" alt="Kitware" width="189" class="sponsor_logo" /></a>
+  <a href="http://www.kitware.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/kitware.png" alt="Kitware" width="189" height="49" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://numfocus.org/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/NumFOCUSfdtn.jpg" alt="NumFOCUS" width="170" class="sponsor_logo" /></a>
+  <a href="http://numfocus.org/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/NumFOCUSfdtn.jpg" alt="NumFOCUS" width="170" height="85" class="sponsor_logo" /></a>
 </div>
 
 <h3 class="sponsor_level">Sprint Sponsor</h3>
 <div class="sponsor_cell">
-  <a href="http://wingware.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/wingware.png" alt="Wingware" width="181" class="sponsor_logo" /></a>
+  <a href="http://wingware.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/wingware.png" alt="Wingware" width="181" height="93" class="sponsor_logo" /></a>
 </div>
 
 
 <h3 class="sponsor_level">Community and Media Sponsors</h3>
 <div class="sponsor_cell">
-  <a href="http://www.computer.org/portal/web/computingnow/cise"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/CiSE.png" alt="CiSE" width="189" class="sponsor_logo" /></a>
+  <a href="http://www.computer.org/portal/web/computingnow/cise"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/CiSE.png" alt="CiSE" width="170" height="51" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://nostarch.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/nsp_logo_black.png" alt="No Starch Press" width="82" class="sponsor_logo" /></a>
+  <a href="http://nostarch.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/nsp_logo_black.png" alt="No Starch Press" width="187" height="29" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://www.oreilly.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/oralogo_cmyk.jpg" alt="O'Reilly" width="189" class="sponsor_logo" /></a>
+  <a href="http://www.oreilly.com"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/oralogo_cmyk.jpg" alt="O'Reilly" width="189" height="36" class="sponsor_logo" /></a>
 </div>
 <div class="sponsor_cell">
-  <a href="http://www.pyladies.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/pyladies_logo.png" alt="pyladies" width="185" class="sponsor_logo" /></a>
+  <a href="http://www.pyladies.com/"><img src="<?php if ($thisPage == 'Admin') {echo "../";} ?>img/sponsor_logos/pyladies_logo.png" alt="pyladies" width="185" height="79" class="sponsor_logo" /></a>
 </div>
