1c8f5c2ac7b61fbe6432cfc48bc760da4ddd0f83
Jim Ivanoff <jim@polarbeardesign.net>
2013-04-12 14:53:02 -0500

    updates to keynotes page

--- keynotes.php
+++ keynotes.php
@@ -27,12 +27,20 @@
   <img src="img/hs_fp.jpg" class="sideslung" width="161" height="154" alt="Fernando Perez" />
   <p>Fernando Perez is a research scientist at the UC Berkeley Henry H. Wheeler Jr. Brain Imaging Center.  After completing a PhD in particle physics and a postdoc in applied mathematics developing numerical algorithms, today he works at the interface between high-level scientific computing tools and the mathematical questions that arise in the analysis of neuroimaging data.  He created IPython while a graduate student in 2001 and continues to lead the project, now as a collaborative effort with a talented team that does all the hard work.  He regularly lectures about scientific computing in Python, and is a member of the Python Software Foundation as well as a founding board member of the NumFOCUS foundation.</p>
 </div>
-<br />
-<br />
+
 <div class="biobox">
   <h2><a id="Schroeder" name="Schroeder">William Schroeder</a></h2>
-  <img src="img/hs_cs.jpg" class="sideslung" width="161" height="154" alt="Coming Soon" />
+  <img src="img/hs_ws.jpg" class="sideslung" width="161" height="154" alt="William Schroeder" />
   <p>CEO of Kitware Inc. - bio coming soon</p>
+</div>
+
+<div class="biobox">
+  <h2><a id="Grisel" name="Grisel">Olivier Grisel</a></h2>
+  <img src="img/hs_og.jpg" class="sideslung" width="161" height="154" alt="Olivier Grisel" />
+  <p>Olivier Grisel is an independent software engineer and expert in
+machine learning specializing in text analytics and natural language
+processing and a regular contributor to the  scikit-learn Machine
+Learning library.</p>
 </div>
 
 </section>
