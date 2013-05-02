6d0b4fe5aaea874c943f490f83ccfe908fd1d1d3
Jim Ivanoff <jim@polarbeardesign.net>
2013-05-02 12:28:50 -0500

    work on schedule page

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -2,12 +2,9 @@
 
 // LIVE SITE
 $db_name = "scipy";
-$connection = @mysql_connect("127.0.0.1","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
-//$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect.");
-<<<<<<< HEAD
+//$connection = @mysql_connect("127.0.0.1","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
+$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect.");
 
-=======
->>>>>>> 8d6d0b7bc28bb4f7b4cbc9540c9aa8f730f2620f
 
 $db = @mysql_select_db($db_name, $connection) or die("Couldn't select database.");
 
