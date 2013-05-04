a7cf69eac45d4149366ee5e43780be8d5d0c9497
Jim Ivanoff <jim@polarbeardesign.net>
2013-05-04 03:36:26 -0500

    db settings change

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -2,8 +2,8 @@
 
 // LIVE SITE
 $db_name = "scipy";
-//$connection = @mysql_connect("127.0.0.1","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
-$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect.");
+$connection = @mysql_connect("127.0.0.1","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
+//$connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect.");
 
 
 $db = @mysql_select_db($db_name, $connection) or die("Couldn't select database.");
