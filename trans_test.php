c07123f8a457cb92cbf78aa374f56ae8e58c6117
Jim Ivanoff <jim@polarbeardesign.net>
2013-06-03 12:05:36 -0500

    update to db connection

--- trans_test.php
+++ trans_test.php
@@ -1,5 +1,21 @@
 <?php
 
+// LIVE SITE
+$db_name = "scipy";
+if ($_SERVER['SERVER_NAME'] == "localhost")
+{
+  $connection = @mysql_connect("127.0.0.1","jri","tensai14") or die("Couldn't Connect.");
+}
+else
+{
+  $connection = @mysql_connect("127.0.0.1","jivanoff","tr4n5c3nd!!") or die("Couldn't Connect.");
+}
+
+
+$db = @mysql_select_db($db_name, $connection) or die("Couldn't select database.");
+
+include('inc/tutorials.php');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -42,53 +58,11 @@
 
 <section id="main-content">
 
-<h1>Transaction Test form</h1>
-
-<form id="formID" class="formular" method="post" action="reg_p3.php">
-
-<input type="hidden" name="tutorialamount" value="400" />
-<input type="hidden" name="conferenceamount" value="300" />
-<input type="hidden" name="sprintamount" value="0" />
-<input type="hidden" name="billTo_email" value="jim@polarbeardesign.net" />
-<input type="hidden" name="shipTo_firstName" value="James" />
-<input type="hidden" name="shipTo_lastName" value="Ivanoff" />
-<input type="hidden" name="affiliation" value="Polar Bear Design" />
-<input type="hidden" name="shipTo_street1" value="2105 Plumbrook Dr" />
-<input type="hidden" name="shipTo_street2" value="" />
-<input type="hidden" name="shipTo_city" value="Austin" />
-<input type="hidden" name="shipTo_state" value="TX" />
-<input type="hidden" name="shipTo_postalCode" value="78746" />
-<input type="hidden" name="shipTo_country" value="US" />
-<input type="hidden" name="billTo_firstName" value="James" />
-<input type="hidden" name="billTo_lastName" value="Ivanoff" />
-<input type="hidden" name="billTo_street1" value="2105 Plumbrook Dr" />
-<input type="hidden" name="billTo_street2" value="" />
-<input type="hidden" name="billTo_city" value="Austin" />
-<input type="hidden" name="billTo_state" value="TX" />
-<input type="hidden" name="billTo_postalCode" value="78746" />
-<input type="hidden" name="billTo_country" value="us" />
-<input type="hidden" name="billTo_phoneNumber" value="5122725474" />
-<input type="hidden" name="participant_type_id " value="1" />
-<input type="hidden" name="conference_id" value="2" />
-<input type="hidden" name="tshirt_type_id"  value="1" />
-<input type="hidden" name="tshirt_size_id " value="4" />
-<input type="hidden" name="ordernumber" value="44444444444" />
-<input type="hidden" name="tutorials" value="on" />
-<input type="hidden" name="conference" value="on" />
-<input type="hidden" name="sprints" value="" />
-<input type="hidden" name="promotion_id" value="" />
-<input type="hidden" name="tutorial_0624_AM" value="100" />
-<input type="hidden" name="tutorial_0624_PM" value="103" />
-<input type="hidden" name="tutorial_0625_AM" value="106" />
-<input type="hidden" name="tutorial_0625_PM" value="109" />
-<input type="hidden" name="promotion_id" value="" />
-
-<div align="center">
-  <input type="submit" name="submit" value="next >>"/>
-</div>
-
-
-</form>
+<a name="tutorials"></a>
+<p class="intra_page_nav"><a href="#top">[ back to top ]</a></p>
+<table id="registrants_table">
+<?php echo $display_tutorials ?>
+</table>
 
 </section>
 <div style="clear:both;"></div>
